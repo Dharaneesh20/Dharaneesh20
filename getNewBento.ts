b3650e0a78d0000c84cb5e31ee0721e5cf0eba2c
Dharaneesh20 <dharaneeshrs777@gmail.com>
2026-01-09 01:56:18 +0530

    Updated URL

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,4 +1,4 @@
-const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=RSD&g=Dharaneesh20&x=Dharaneesh13581&l=dharaneeshrs-clouddev&i=https%3A%2F%2Fi.ibb.co%2FMx8LQjj6%2Fwp8963710-watch-dogs-2-4k-pc-wallpapers.jpg&p=https%3A%2F%2Fportfoliorsd.vercel.app%2F&z=5a2a8";
+const apiUrl = "https://opbento.vercel.app/api/bento?n=RSD&g=Dharaneesh20&x=Dharaneesh13581&l=dharaneeshrs-clouddev&i=https%3A%2F%2Fi.ibb.co%2FMx8LQjj6%2Fwp8963710-watch-dogs-2-4k-pc-wallpapers.jpg&p=https%3A%2F%2Fportfoliorsd.vercel.app%2F&z=5a2a8";
 interface BentoResponse {
   url: string;
 }
